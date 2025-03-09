bbbc377da57e7a35c720bc9e53b6ed8d4a4cf416
starky29 <andruwkakz@gmail.com>
2025-03-09 19:08:58 +0300

    add atlantis srv

--- bucket/variables.tf
+++ bucket/variables.tf
@@ -16,7 +16,12 @@ variable "folder_id" {
 
 variable "ssh_public_key" {
   type    = string
-  default = "~/.ssh/id_ed25519.pub"
+}
+variable "github_token" {
+  type    = string
+}
+variable "webhook_token" {
+  type    = string
 }
 variable "zone" {
   description = "Зона доступности"
@@ -24,29 +29,8 @@ variable "zone" {
   default     = "ru-central1-a"
 }
 
-variable "ssh_public_key_path" {
-  description = "Путь к публичному SSH ключу"
-  type        = string
-  default     = "~/.ssh/id_ed25519.pub"
-}
-
 variable "vpc_name" {
   description = "Имя виртуальной сети"
   type        = string
   default     = "vpc"
-}
-
-variable "subnet_public_name" {
-  description = "Имя публичной подсети"
-  type        = string
-  default     = "public"
-}
-
-variable "subnet_private_name" {
-  description = "Имя приватной подсети"
-  type        = string
-  default     = "private"
-}
-variable "default_zone" {
-  default = "ru-central1-a"
 }
